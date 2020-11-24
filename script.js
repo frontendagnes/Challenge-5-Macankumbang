69a5d8ba413f405ae510d8905678ff97ea29cf4b
Agnieszka Kaminska <agnieszka.kaminska@ksiegarnia.edu.pl>
2020-11-24 12:52:20 +0100

    portofolio add js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-document.querySelector("div").addEventListener("click", function() {
-    console.log("mam cię")
-    console.log(this)
+window.addEventListener('DOMContentLoaded', () =>{
+ 
+ //#region  portofolio obracenie zdjęć
+ const flexItems = document.querySelectorAll('.flexbox-img__descritpion');
+ const flexItem = document.querySelectorAll('.flexbox-img__item');
+ 
+for(let i = 0; i <= flexItem.length; i++){
+    flexItem[i].addEventListener('mouseenter', () =>{
+        flexItems[i].classList.remove('flexbox-img__noActive')
+    })
+
+    flexItem[i].addEventListener('mouseleave', () => {
+        flexItems[i].classList.add('flexbox-img__noActive')
+    })
+}
+//#endregion
+
+
 })
+
