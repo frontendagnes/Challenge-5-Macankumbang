43087da79b82e0bff1d083021d6404995a193d89
Agnieszka Kaminska <agnieszka.kaminska@ksiegarnia.edu.pl>
2020-11-27 10:03:28 +0100

    remove js portofolio

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-
 import menuFixed from "./js/menu.js"
-import leftRightSide from "./js/portofolio.js";
-import height100 from "./js/height100.js";
-import eye from "./js/eye.js";
-import slider from "./js/slider.js";
-import arrowUp from "./js/arrowUp.js"
-
-height100();
-eye();
-leftRightSide();
-slider();
 menuFixed();
+
+import height100 from "./js/height100.js";
+height100();
+
+import eye from "./js/eye.js";
+eye();
+
+import slider from "./js/slider.js";
+slider();
+
+import arrowUp from "./js/arrowUp.js"
 arrowUp();
 
